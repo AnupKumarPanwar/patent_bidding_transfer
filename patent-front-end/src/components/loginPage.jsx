7dbe6ab95798525d59091d1ad71696947a8a4ce1
Lovepreet Singh <lovepreet.singh61182@gmail.com>
2019-01-10 16:51:45 +0530

    loginpage added

--- patent-front-end/src/components/loginPage.jsx
+++ patent-front-end/src/components/loginPage.jsx
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
-
+import "./css/loginCss.css";
 class Login extends Component {
   state = {};
 
   render() {
     return (
-      <div>
-        <h3>Login</h3>
-        <form className='form-group'>
-          <p className='m-2'>UserId</p>
-          <input type='text' className='form-control' />
-          <p className='m-2'>Password</p>
-          <input type='password' className='form-control' />
+      <div className='form-container'>
+        <strong align='center'>
+          <h2>Login</h2>
+        </strong>
+        <form className='form-wrapper'>
+          <strong className='m-2'>UserId</strong>
+          <input type='text' className='form-control' placeholder='Userid' />
+          <strong className='m-2'>Password</strong>
+          <input
+            type='password'
+            className='form-control'
+            placeholder='password'
+          />
         </form>
-        <div>
-          <button className='btn btn-primary'>Login</button>
-          <button className='btn btn-primary m-2'>Register</button>
+        <br />
+        <div className='login-button'>
+          <button className='btn btn-primary btn-block '>Login</button>
+
+          {/* <button className='btn btn-primary m-2'>Register</button> */}
         </div>
       </div>
     );
