3195d7f495f38706585add69d3234d9cc20659ff
Anup Kumar Panwar <1anuppanwar@gmail.com>
2019-02-18 19:24:19 +0530

    search front end

--- patent-front-end/src/components/manage/searchPage.jsx
+++ patent-front-end/src/components/manage/searchPage.jsx
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
 import { TextField, Button, TabsContainer, Tabs, Tab } from 'react-md';
+import service from '../../services/patentService';
+import {
+    DataTable,
+    TableHeader,
+    TableBody,
+    TableRow,
+    TableColumn,
+} from 'react-md';
+
+import { MdArrowDownward } from 'react-icons/md';
+import { Link } from "react-router-dom";
 import axios from "axios";
 import controller from '../../controller';
 import { patentForAuctionThunk } from "../../store/thunk/managePatentThunk";
@@ -14,6 +25,17 @@ class AuctionForm extends Component {
 
     handleFieldChange = (value, event) => {
         this.setState({ [event.target.id]: value })
+        service.searchService({ query: value })
+            .then((res) => {
+                var patents = res.message.patents;
+                var users = res.message.users;
+                this.setState({ patents, users });
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        console.log(this.state.query);
     }
 
     componentDidMount() {
@@ -26,6 +48,32 @@ class AuctionForm extends Component {
 
     render() {
 
+        const patentRows = this.state.patents.map(({ patentName, patentType }, index) => (
+            <TableRow key={index} >
+                <TableColumn>{patentName}</TableColumn>
+                <TableColumn>{patentType}</TableColumn>
+            </TableRow>
+        ));
+
+        const userRows = this.state.users.map(({ name, email }, index) => (
+            <TableRow key={index} >
+                <TableColumn>{name}</TableColumn>
+                <TableColumn>{email}</TableColumn>
+            </TableRow>
+        ));
+
+        const noResultsFound =
+            <TableRow>
+                <TableColumn>No result found</TableColumn>
+            </TableRow>
+            ;
+
+        const noSearchQuery =
+            <TableRow>
+                <TableColumn>Type a search query</TableColumn>
+            </TableRow>
+            ;
+
         return (
             <div className="md-grid">
                 <TextField
@@ -34,15 +82,24 @@ class AuctionForm extends Component {
                     onChange={this.handleFieldChange}
                     className="md-cell md-cell--12"
                     placeholder="Search"
+                    autoComplete={false}
                 />
 
                 <TabsContainer panelClassName="md-grid" className="md-cell md-cell--12" colored>
                     <Tabs tabId="simple-tab">
                         <Tab label="Patents">
-                            <h3>Hello, World!</h3>
+                            <DataTable baseId="patent" plain={true} responsive >
+                                <TableBody>
+                                    {this.state.patents.length > 0 ? patentRows : this.state.query.length > 0 ? noResultsFound : noSearchQuery}
+                                </TableBody>
+                            </DataTable>
                         </Tab>
                         <Tab label="Users">
-                            <h3>Now look at me!</h3>
+                            <DataTable baseId="patent" plain={true} responsive >
+                                <TableBody>
+                                    {this.state.users.length > 0 ? userRows : this.state.query.length > 0 ? noResultsFound : noSearchQuery}
+                                </TableBody>
+                            </DataTable>
                         </Tab>
                     </Tabs>
                 </TabsContainer>
